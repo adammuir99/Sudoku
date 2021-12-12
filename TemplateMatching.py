a98a4e760f9b64c7c1539639fc30b4aeaca2f6c8
Adam Muir <68184047+adammuir99@users.noreply.github.com>
2021-12-11 20:27:28 -0500

    Number detection now functional

--- TemplateMatching.py
+++ TemplateMatching.py
@@ -1,7 +1,11 @@
+from time import perf_counter_ns
 import cv2
 import numpy as np
 import imutils
- 
+import pytesseract
+
+# First match the image with the sudoku board template
+# to locate the sudoku board within the template ###########################################################################################
 # Read the template
 template = cv2.imread('template.jpg')
 # Convert the template to grayscale
@@ -49,8 +53,8 @@ for scale in np.linspace(0.2, 1.0, 20)[::-1] :
 	clone = np.dstack([edged, edged, edged])
 	cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
 		(maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
-	cv2.imshow("Visualize", clone)
-	cv2.waitKey(0)
+	#cv2.imshow("Visualize", clone)
+	#cv2.waitKey(0)
 
 	# if we have found a new maximum correlation value, then update
 	# the bookkeeping variable
@@ -66,5 +70,30 @@ for scale in np.linspace(0.2, 1.0, 20)[::-1] :
 # draw a bounding box around the detected result and display the image
 cv2.rectangle(img_rgb, (startX, startY), (endX, endY), (0, 0, 255), 2)
 cv2.imshow("Image", img_rgb)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Next we will detect the numbers that are found within the bounding box #################################################################
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+# Crop the image to only read the sudoku box
+img_crop = img_rgb[startY:endY, startX:endX].copy()
+
+# Resizing to a larger image will help tesseract detect the numbers
+img_crop = cv2.resize(img_crop, None, fx=1.3, fy=1.3, interpolation=cv2.INTER_CUBIC)
+
+# Tesseract only works with RGB images, OpenCV uses BGR
+img_text = cv2.cvtColor(img_crop, cv2.COLOR_BGR2RGB)
+
+hImg, wImg,_ = img_text.shape
+boxes = pytesseract.image_to_boxes(img_text, config="--psm 6 -c tessedit_char_whitelist=0123456789")
+for b in boxes.splitlines() :
+    b = b.split(' ')
+    print(b)
+    x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
+    cv2.rectangle(img_text, (x,hImg-y), (w,hImg-h), (0,0,255), 1)
+    cv2.putText(img_text, b[0], (x, hImg-y+25), cv2.FONT_HERSHEY_PLAIN, 1, (50, 50, 255), 2)
+
+cv2.imshow("Text", img_text)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
